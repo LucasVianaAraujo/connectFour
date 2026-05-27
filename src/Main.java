f4ca6c154cb9a7d08bfc8e08a46f3cd49e3c39ee
Lucas Viana <luks79180@gmail.com>
2026-05-27 11:30:45 -0300

    1.0

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("== CONNECT FOUR ==\n\nPressione 'Enter' para jogar: ");
+        sc.nextLine();
+
+        int simboloJogador = simboloJogador();
+        String nomeJogador = nomeJogador();
+    }
+
+    static void regrasDoJogo() {
+        Scanner sc = new Scanner(System.in);
+        // regras do jogo para um melhor dinamismo e interação
+    }
+
+    static void connectFour() {
+        Scanner sc = new Scanner(System.in);
+        // o jogo em si
+    }
+
+    static int simboloJogador() {
+        System.out.printf("Escolha um símbolo: \n\n1 - X\n\n2 - O\n\nSímbolo: ");
+
+        Scanner sc = new Scanner(System.in);
+        int simbolo = sc.nextInt();
+
+        return simbolo;
+    }
+
+    static String nomeJogador() {
+        System.out.printf("\nNome do jogador: ");
+
+        Scanner sc = new Scanner(System.in);
+        String nome1 = sc.nextLine();
+
+        return nome1;
+    }
+}
